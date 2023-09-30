cf90413c71f6b203466404795cb85620c88d2d2a
Angel Jahir Apodaca Gutierrez <angel.apodaca7981@alumnos.udg.mx>
2023-09-30 13:06:56 -0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,31 @@
 jQuery(document).ready(function($) {
 
-    $('.level-bar-inner').css('width', '0');
-    
+    $('.level-bar-inner').css('width','0');
+
     $(window).on('load', function() {
 
         $('.level-bar-inner').each(function() {
-        
+
             var itemWidth = $(this).data('level');
-            
+
             $(this).animate({
+
                 width: itemWidth
+
             }, 800);
-            
-        });
-
+      });
     });
-   
-    
-
 });
+
+$(document).ready(function() {
+    $('.image-carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true,
+      autoplay: true,
+      autoplaySpeed: 3000
+    });
+  });
