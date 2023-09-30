d0ed1cdee78a0a40ee53e86950d3bfa26d0ebf7f
Angel Jahir Apodaca Gutierrez <angel.apodaca7981@alumnos.udg.mx>
2023-09-30 14:30:29 -0600

    Actividad 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,21 @@
 jQuery(document).ready(function($) {
 
-    $('.level-bar-inner').css('width','0');
-
+    $('.level-bar-inner').css('width', '0');
+    
     $(window).on('load', function() {
 
         $('.level-bar-inner').each(function() {
-
+        
             var itemWidth = $(this).data('level');
-
+            
             $(this).animate({
-
                 width: itemWidth
-
             }, 800);
-      });
+            
+        });
+
     });
+   
+    
+
 });
-
-const carousel = document.querySelector('.image-carousel');
-const prevButton = carousel.querySelector('.prev-button');
-const nextButton = carousel.querySelector('.next-button');
-const images = carousel.querySelectorAll('.image-container');
-let currentIndex = 0;
-
-function showImage(index) {
-  images.forEach((image, i) => {
-    if (i === index) {
-      image.style.display = 'block';
-    } else {
-      image.style.display = 'none';
-    }
-  });
-}
-
-function showNextImage() {
-  currentIndex++;
-  if (currentIndex >= images.length) {
-    currentIndex = 0;
-  }
-  showImage(currentIndex);
-}
-
-function showPrevImage() {
-  currentIndex--;
-  if (currentIndex < 0) {
-    currentIndex = images.length - 1;
-  }
-  showImage(currentIndex);
-}
-
-
-prevButton.addEventListener('click', showPrevImage);
-nextButton.addEventListener('click', showNextImage);
-setInterval(showNextImage, 3000);
-
-showImage(currentIndex);
